08c7ebfcd5683cef51dc6f250d1f6d376267acae
Alan Guedes <alanlivio@outlook.com>
2022-05-13 12:49:26 +0100

    add words_around_change and fix test

--- tests/docxreviews2txt_test.py
+++ tests/docxreviews2txt_test.py
@@ -1,7 +1,7 @@
-from docxreviews2txt.docxreviews2txt import main
-import unittest
-import io
 import contextlib
+import io
+import unittest
+import docxreviews2txt
 
 
 class TestCase(unittest.TestCase):
@@ -9,19 +9,22 @@ class TestCase(unittest.TestCase):
         lorem_ipsum = [
             '# Typos and rewriting suggestions',
             '- sit amet, consectetur  -> sit amet, consectetur Lorem ipsum',
-            '- sit amet, consectetur adipiscing elit, sed do -> sit amet, consectetur elit, sed do',
-            '- sit amet, consectetur adipiscing elit, sed -> sit amet, consectetur adipiscings elit, sed',
-            '- enim ad minim veniam, quis nostrud -> enim ad minim do veniam, quis nostrud',
+            '- sit amet, consectetur adipiscing elit, sed do eiusmod -> sit amet, consectetur elit, sed do eiusmod',
+            '- sit amet, consectetur adipiscing elit, sed do -> sit amet, consectetur adipiscings elit, sed do',
+            '- enim ad minim veniam, quis nostrud exercitation -> enim ad minim do veniam, quis nostrud exercitation',
             '- enim ad minim veniam -> enim ad minim Lorem veniam',
-            '- veniam, quis nostrud -> veniam ipsum, quis nostrud',
-            '- sit amet, consectetur adipiscing elit, sed do -> sit amet, consectetur elit, sed do'
+            '- veniam, quis nostrud exercitation -> veniam ipsum, quis nostrud exercitation',
+            '- sit amet, consectetur adipiscing elit, sed do eiusmod -> sit amet, consectetur elit, sed do eiusmod',
         ]
         # redirect stdout (https://stackoverflow.com/questions/54824018/get-output-of-a-function-as-string)
         f = io.StringIO()
         with contextlib.redirect_stdout(f):
             # pass args to main (https://jugmac00.github.io/blog/testing-argparse-applications-the-better-way/)
-            main(["tests/lorem_ipsum.docx"])
-            self.assertEqual(lorem_ipsum, f.getvalue().split('\n')[:-1])
+            docx_reviews = docxreviews2txt.DocxReviews("tests/lorem_ipsum.docx", True)
+            docx_reviews.parse()
+            output = f.getvalue().split('\n')[:-1]
+            self.assertEqual(len(lorem_ipsum), len(output))
+            self.assertEqual(lorem_ipsum, output)
 
 
 if __name__ == '__main__':
