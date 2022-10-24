d2542f4075ce11cf065384a30de967317eeb47ad
Alan Guedes <alanlivio@gmail.com>
2022-10-24 16:50:57 +0100

    add Pylance suggestions

--- docxreviews2txt/docxreviews2txt.py
+++ docxreviews2txt/docxreviews2txt.py
@@ -19,7 +19,7 @@ DEFAULT_WORDS_AROUND_CHANGE = 4
 
 
 class DocxReviews:
-    def __init__(self, file_docx, verbose, words_around_change=DEFAULT_WORDS_AROUND_CHANGE):
+    def __init__(self, file_docx, verbose, words_around_change=DEFAULT_WORDS_AROUND_CHANGE) -> None:
         self.verbose = verbose
         self.reviews = []
         self.file_docx = file_docx
@@ -39,17 +39,17 @@ class DocxReviews:
         self.docxZip = zipfile.ZipFile(temp_path, mode="r")
         self.paragraphs = Document(temp_path).paragraphs
 
-    def str_deltext_elms(self, child):
+    def str_deltext_elms(self, child) ->str:
         x = [text.text for text in child.findall(
             './/w:delText', NS_MAP)]
         return "".join(x)
 
-    def str_t_elms(self, child):
+    def str_t_elms(self, child)->str:
         x = [text.text for text in child.findall(
             './/w:t', NS_MAP)]
         return "".join(x)
 
-    def str_left_t_elms(self, root_p, index):
+    def str_left_t_elms(self, root_p, index)->str:
         left_ar = []
         for i in range(index, 0, -1):
             if (root_p[i].tag == ET_INS):
@@ -60,7 +60,7 @@ class DocxReviews:
             break
         return " ".join(left_ar)
 
-    def str_right_t_elms(self, root_p, index):
+    def str_right_t_elms(self, root_p, index)->str:
         right_ar = []
         for i in range(index, len(root_p)):
             if (root_p[i].tag == ET_INS):
@@ -71,14 +71,14 @@ class DocxReviews:
             break
         return " ".join(right_ar)
 
-    def reviews_append(self, text):
+    def reviews_append(self, text) -> None:
         if not len(text):
             return
         self.reviews.append(text)
         if self.verbose:
             print(self.reviews[-1])
 
-    def parse(self):
+    def parse(self) -> None:
         # comments
         if 'word/comments.xml' in [member.filename for member in self.docxZip.infolist()]:
             commentsXML = self.docxZip.read('word/comments.xml')
