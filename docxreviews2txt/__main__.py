7efdf35392567e7f9a3c36d7419ae5ba0b73f8eb
Alan Guedes <a.guedes@ucl.ac.uk>
2023-04-01 17:26:56 +0100

    update requirements and code format

--- docxreviews2txt/__main__.py
+++ docxreviews2txt/__main__.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import argparse
 import pathlib
 import sys
@@ -7,19 +5,25 @@ import sys
 import docxreviews2txt
 
 
-def main()  -> None:
-    parser = argparse.ArgumentParser(prog='docxreviews2txt',
-        description="Extract review changes and comments from a docx file as plain text.")
-    parser.add_argument("docx", help="input docx", type=pathlib.Path)
-    parser.add_argument('--save_p_xml', help='also save extracted Docx paragraphs as xml for debugging', action="store_true")
-    parser.add_argument('--version', help='show version', action='version',
-                        version='%(prog)s ' + docxreviews2txt.__version__)
-    argv = sys.argv[1:]
-    args = parser.parse_args(argv)
-    docx_reviews = docxreviews2txt.DocxReviews(args.docx)
-    docx_reviews.save_reviews_to_file()
-    if args.save_p_xml:
-        docx_reviews.save_xml_p_elems()
+def main() -> None:
+  parser = argparse.ArgumentParser(
+      prog='docxreviews2txt',
+      description="Extract review changes and comments from a docx file as plain text.")
+  parser.add_argument("docx", help="input docx", type=pathlib.Path)
+  parser.add_argument('--save_p_xml',
+                      help='also save extracted Docx paragraphs as xml for debugging',
+                      action="store_true")
+  parser.add_argument('--version',
+                      help='show version',
+                      action='version',
+                      version='%(prog)s ' + docxreviews2txt.__version__)
+  argv = sys.argv[1:]
+  args = parser.parse_args(argv)
+  docx_reviews = docxreviews2txt.DocxReviews(args.docx)
+  docx_reviews.save_reviews_to_file()
+  if args.save_p_xml:
+    docx_reviews.save_xml_p_elems()
+
 
 if __name__ == "__main__":
-    main()
+  main()
