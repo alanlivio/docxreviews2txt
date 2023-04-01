553008e65a88391a02c5ac439adf8e6a13f656b3
Alan Guedes <alanlivio@outlook.com>
2023-04-01 17:58:11 +0100

    tests resource-aware and code format

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 with open('requirements.txt') as f:
-    required = f.read().splitlines()
+  required = f.read().splitlines()
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+  long_description = fh.read()
 
 setuptools.setup(
     name="docxreviews2txt",
@@ -23,12 +23,11 @@ setuptools.setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3", ],
+        "Programming Language :: Python :: 3",
+    ],
     author_email="alanlivio@gmail.com",
-    description="Command line tool to extract review changes and comments from a docx file as plain text.",
-    entry_points = {
-        "console_scripts": [
-            "docxreviews2txt = docxreviews2txt.__main__:main",
-        ]
-    }
-)
+    description=
+    "Command line tool to extract review changes and comments from a docx file as plain text.",
+    entry_points={"console_scripts": [
+        "docxreviews2txt = docxreviews2txt.__main__:main",
+    ]})
