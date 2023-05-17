9ab1ad90e045a916ed8c3c5312bd8c7129c51671
Alan Guedes <a.guedes@ucl.ac.uk>
2023-05-17 20:02:24 +0100

    rm --save_xml_p_elems

--- docxreviews2txt/__main__.py
+++ docxreviews2txt/__main__.py
@@ -10,9 +10,6 @@ def main() -> None:
       prog='docxreviews2txt',
       description="Extract review changes and comments from a docx file as plain text.")
   parser.add_argument("docx", help="input docx", type=pathlib.Path)
-  parser.add_argument('--save_p_xml',
-                      help='also save extracted Docx paragraphs as xml for debugging',
-                      action="store_true")
   parser.add_argument('--version',
                       help='show version',
                       action='version',
@@ -21,9 +18,6 @@ def main() -> None:
   args = parser.parse_args(argv)
   docx_reviews = docxreviews2txt.DocxReviews(args.docx)
   docx_reviews.save_reviews_to_file()
-  if args.save_p_xml:
-    docx_reviews.save_xml_p_elems()
-
 
 if __name__ == "__main__":
   main()
