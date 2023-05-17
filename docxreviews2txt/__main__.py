78ef6f9c6fe8e0bdcf7e2437ceb4ed665fd8bd19
Alan Guedes <alanlivio@outlook.com>
2023-05-17 20:12:20 +0100

    add -nwords

--- docxreviews2txt/__main__.py
+++ docxreviews2txt/__main__.py
@@ -2,7 +2,7 @@ import argparse
 import pathlib
 import sys
 
-import docxreviews2txt
+from docxreviews2txt import DocxReviews, DEFAULT_NWORDS, __version__
 
 
 def main() -> None:
@@ -10,14 +10,20 @@ def main() -> None:
       prog='docxreviews2txt',
       description="Extract review changes and comments from a docx file as plain text.")
   parser.add_argument("docx", help="input docx", type=pathlib.Path)
+  parser.add_argument('-nwords',
+                      nargs='?',
+                      type=int,
+                      default=DEFAULT_NWORDS,
+                      help=f'words around each change (default: {DEFAULT_NWORDS})')
   parser.add_argument('--version',
                       help='show version',
                       action='version',
-                      version='%(prog)s ' + docxreviews2txt.__version__)
+                      version='%(prog)s ' + __version__)
   argv = sys.argv[1:]
   args = parser.parse_args(argv)
-  docx_reviews = docxreviews2txt.DocxReviews(args.docx)
+  docx_reviews = DocxReviews(file_docx=args.docx, nwords=args.nwords)
   docx_reviews.save_reviews_to_file()
+
 
 if __name__ == "__main__":
   main()
