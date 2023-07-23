7a56e1b607be2b94a872558a142f643a61961349
Alan Guedes <a.guedes@ucl.ac.uk>
2023-07-23 22:11:30 +0100

    refact tests

--- tests/test_docxreviews2txt.py
+++ tests/test_docxreviews2txt.py
@@ -15,21 +15,15 @@ XML_EXPECTED = "tests/lorem_ipsum_expected.xml"
 
 class TestCase(unittest.TestCase):
 
-  def test_lorem_ipsum_cli(self) -> None:
-    # redirect stdout  https://stackoverflow.com/questions/54824018/get-output-of-a-function-as-string
-    output = StringIO()
-    with contextlib.redirect_stdout(output):
-      docx_reviews = docxreviews2txt.DocxReviews(DOCX)
-      docx_reviews.save_xml_p_elems()
-      cli_l = output.getvalue().split('\n')[:-1]
-      cli_expected_l = [f'txt reviews at {pathlib.Path(abspath(TXT_OUT)).as_uri()}']
-      cli_expected_l = [f'xml paragraphs at {pathlib.Path(abspath(XML_OUT)).as_uri()}']
-      self.assertEqual(cli_l, cli_expected_l)
-
   def test_lorem_ipsum_txt(self) -> None:
     assert exists(TXT_EXPECTED)
     docx_reviews = docxreviews2txt.DocxReviews(DOCX)
-    docx_reviews.save_reviews_to_file()
+    output = StringIO()
+    with contextlib.redirect_stdout(output):
+      docx_reviews.save_reviews_to_file()
+      cli_l = output.getvalue().split('\n')[:-1]
+      cli_expected_l = [f'txt reviews at {pathlib.Path(abspath(TXT_OUT)).as_uri()}']
+      self.assertEqual(cli_l, cli_expected_l)
     assert exists(TXT_OUT)
     with open(TXT_OUT) as f:
       ouput_l = f.read().splitlines()
@@ -37,17 +31,21 @@ class TestCase(unittest.TestCase):
       expected_l = f.read().splitlines()
     self.assertEqual(ouput_l, expected_l)
 
-  def test_lorem_ipsum_p_xml(self) -> None:
+  def test_lorem_ipsum_xml_p_elems(self) -> None:
     assert exists(XML_EXPECTED)
     docx_reviews = docxreviews2txt.DocxReviews(DOCX)
-    docx_reviews.save_xml_p_elems()
+    output = StringIO()
+    with contextlib.redirect_stdout(output):
+      docx_reviews.save_xml_p_elems()
+      cli_l = output.getvalue().split('\n')[:-1]
+      cli_expected_l = [f'xml paragraphs at {pathlib.Path(abspath(XML_OUT)).as_uri()}']
+      self.assertEqual(cli_l, cli_expected_l)
     assert exists(XML_OUT)
     with open(XML_OUT) as f:
       ouput_l = f.read().splitlines()
     with open(XML_EXPECTED) as f:
       expected_l = f.read().splitlines()
     self.assertEqual(ouput_l, expected_l)
-
 
 if __name__ == '__main__':
   unittest.main()
